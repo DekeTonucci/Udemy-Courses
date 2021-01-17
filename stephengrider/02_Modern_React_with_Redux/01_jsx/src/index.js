3fc6af738b43d17c95cce46351eafde944659b74
Deke <deketonucci@google.com>
2021-01-17 13:42:01 -0500

    JSX basics

--- stephengrider/02_Modern_React_with_Redux/01_jsx/src/index.js
+++ stephengrider/02_Modern_React_with_Redux/01_jsx/src/index.js
@@ -1,17 +1,30 @@
+// Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// Hot reload index.js of CRA:
+if (module.hot) {
+  module.hot.accept();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a React component
+const App = function () {
+  // const buttonText = "Click me";
+  const labelText = ["Enter ", "Name: "];
+  const buttonText = { text: "Click me!" };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the React component and show in browser
+ReactDOM.render(<App />, document.querySelector("#root"));
